6726ea84811680297666d7f910248447f5af911f
Sahil Sendre <ss.sahilshendre02@gmail.com>
2024-04-03 19:18:29 +0530

    third commit

--- src/Pages/Benefits.jsx
+++ src/Pages/Benefits.jsx
@@ -1,49 +1,120 @@
-import React from 'react'
-import time from '../image/time.png'
-import insight from '../image/insight.png';
-import emp from '../image/emp.png';
-import scale from '../image/scale.png';
+// import React from "react";
+// import time from "../image/time.png";
+// import insight from "../image/insight.png";
+// import emp from "../image/emp.png";
+// import scale from "../image/scale.png";
+// function Benefits() {
+//   return (
+//     <div className="w-full h-[25rem]  benefits ">
+//       <div className="features h-40 flex justify-center items-center">
+//         <p className="text-black font-semibold text-lg sm:text-2xl md:text-3xl lg:text-3xl  pb-8">
+//           Benefits of Sortboxs HRM
+//         </p>
+//       </div>
+//       <div className="w-[80%] h-[30%] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-5 mx-auto">
+//         <div className="shadow-xl flex flex-col justify-center bg-white p-4 gap-2 hover:bg-gradient-to-r from-yellow-200 to-orange-400">
+//           <div className="flex flex-row gap-2 " >
+//             <img src={time} alt="time" />
+//             <p className="text-md font-semibold text-[#FC8320]">Time Saving</p>
+//           </div>
+//           <p className="text-black text-[0.7rem] sm:text-xs font-medium">
+//             Automate repetitive HR tasks to save time for strategic initiatives.
+//           </p>
+//         </div>
+//         <div className="shadow-xl flex flex-col justify-center bg-white p-4 gap-2 hover:bg-gradient-to-r from-cyan-200 to-cyan-500 ">
+//           <div className="flex flex-row gap-2 ">
+//             <img src={insight} alt="insight" />
+//             <p className="text-md font-semibold text-[#16B7C3] ">
+//               Insightful Reporting
+//             </p>
+//           </div>
+//           <p className="text-black text-[0.7rem] sm:text-xs font-medium ">
+//             Automate repetitive HR tasks to save time for strategic initiatives.
+//           </p>
+//         </div>
+//         <div className="shadow-xl flex flex-col justify-center bg-white p-4 gap-2 hover:bg-gradient-to-r from-[#7fa7ff] to-[#407bff]">
+//           <div className="flex flex-row gap-2">
+//             <img src={emp} alt="emp" />
+//             <p className="text-md font-semibold text-[#407BFF]">
+//               Employee Engagement
+//             </p>
+//           </div>
+//           <p className="text-black text-[0.7rem] sm:text-xs font-medium">
+//             Automate repetitive HR tasks to save time for strategic initiatives.
+//           </p>
+//         </div>
+//         <div className="shadow-xl flex flex-col justify-center bg-white p-4 gap-2 hover:bg-gradient-to-r from-[#81fa7f] to-[#16f112]">
+//           <div className="flex flex-row gap-2">
+//             <img src={scale} alt="scale" />
+//             <p className="text-md font-semibold text-[#16F112]">Scalability</p>
+//           </div>
+//           <p className="text-black text-[0.7rem] sm:text-xs font-medium">
+//             Automate repetitive HR tasks to save time for strategic initiatives.
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Benefits;
+import React from "react";
+import time from "../image/time.png";
+import insight from "../image/insight.png";
+import emp from "../image/emp.png";
+import scale from "../image/scale.png";
+
 function Benefits() {
   return (
-    <div className='w-full h-[25rem]  benefits '>
-        <div className='features h-40 flex justify-center items-center'>
-            <p className='text-black font-semibold text-lg sm:text-2xl md:text-3xl lg:text-3xl  pb-8'>
-            Benefits of Sortboxs HRM
-            </p>
+    <div className="w-full h-[25rem] benefits">
+      <div className="features h-40 flex justify-center items-center">
+        <p className="text-black font-semibold text-lg sm:text-2xl md:text-3xl lg:text-3xl  pb-8">
+          Benefits of Sortboxs HRM
+        </p>
+      </div>
+      <div className="w-[80%] h-[30%] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-5 mx-auto">
+        <div className="shadow-xl flex flex-col justify-center bg-white p-4 gap-2 hover:bg-gradient-to-r from-yellow-200 to-orange-400 hover:text-white  benefits_css">
+          <div className="flex flex-row gap-2">
+            <img src={time} alt="time" />
+            <p className="text-md font-semibold text-[#FC8320]">Time Saving</p>
+          </div>
+          <p className="text-black text-[0.7rem] sm:text-xs font-medium">
+            Automate repetitive HR tasks to save time for strategic initiatives.
+          </p>
         </div>
-        <div className='w-[80%] h-[30%] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-5 mx-auto'>
-            <div className='shadow-xl flex flex-col justify-center bg-white p-4 gap-2'>   
-                <div className='flex flex-row gap-2'>
-                    <img src={time} alt="time" />
-                    <p className='text-md font-semibold text-[#FC8320]'>Time Saving</p>
-                </div>
-                <p className='text-black text-[0.7rem] sm:text-xs font-medium'>Automate repetitive HR tasks to save time for strategic initiatives.</p>
-            </div>
-            <div className='shadow-xl flex flex-col justify-center bg-white p-4 gap-2'>   
-                <div className='flex flex-row gap-2'>
-                    <img src={insight} alt="insight" />
-                    <p className='text-md font-semibold text-[#16B7C3]'>Insightful Reporting</p>
-                </div>
-                <p className='text-black text-[0.7rem] sm:text-xs font-medium'>Automate repetitive HR tasks to save time for strategic initiatives.</p>
-            </div>
-            <div className='shadow-xl flex flex-col justify-center bg-white p-4 gap-2'>   
-                <div className='flex flex-row gap-2'>
-                    <img src={emp} alt="emp" />
-                    <p className='text-md font-semibold text-[#407BFF]'>Employee Engagement</p>
-                </div>
-                <p className='text-black text-[0.7rem] sm:text-xs font-medium'>Automate repetitive HR tasks to save time for strategic initiatives.</p>
-            </div>
-            <div className='shadow-xl flex flex-col justify-center bg-white p-4 gap-2'>   
-                <div className='flex flex-row gap-2'>
-                    <img src={scale} alt="scale" />
-                    <p className='text-md font-semibold text-[#16F112]'>Scalability</p>
-                </div>
-                <p className='text-black text-[0.7rem] sm:text-xs font-medium'>Automate repetitive HR tasks to save time for strategic initiatives.</p>
-            </div>
+        <div className="shadow-xl flex flex-col justify-center bg-white p-4 gap-2 hover:bg-gradient-to-r from-cyan-200 to-cyan-500 hover:text-white benefits_css">
+          <div className="flex flex-row gap-2">
+            <img src={insight} alt="insight" />
+            <p className="text-md font-semibold text-[#16B7C3]">Insightful Reporting</p>
+          </div>
+          <p className="text-black text-[0.7rem] sm:text-xs font-medium">
+          Generate comprehensive reports 
+quickly for informed decision-making.
+          </p>
         </div>
-
+        <div className="shadow-xl flex flex-col justify-center bg-white p-4 gap-2 hover:bg-gradient-to-r from-[#7fa7ff] to-[#407bff] hover:text-white benefits_css">
+          <div className="flex flex-row gap-2">
+            <img src={emp} alt="emp" />
+            <p className="text-md font-semibold text-[#407BFF]">Employee Engagement</p>
+          </div>
+          <p className="text-black text-[0.7rem] sm:text-xs font-medium">
+          Foster positive employee experiences, 
+leading to increased engagement.
+          </p>
+        </div>
+        <div className="shadow-xl flex flex-col justify-center bg-white p-4 gap-2 hover:bg-gradient-to-r from-[#81fa7f] to-[#16f112] hover:text-white benefits_css">
+          <div className="flex flex-row gap-2">
+            <img src={scale} alt="scale" />
+            <p className="text-md font-semibold text-[#16F112]">Scalability</p>
+          </div>
+          <p className="text-black text-[0.7rem] sm:text-xs font-medium">
+          Adaptable solutions to accommodate 
+organizational growth effortlessly.
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Benefits
+export default Benefits;
--- src/Pages/Modules.jsx
+++ src/Pages/Modules.jsx
@@ -1,111 +1,3 @@
-// import React from 'react'
-// import mod1 from '../image/1.png'
-// import mod2 from '../image/2.png'
-// import mod3 from '../image/3.png'
-// import mod4 from '../image/4.png'
-// import mod5 from '../image/5.png'
-// import mod6 from '../image/6.png'
-// import mod7 from '../image/7.png'
-// import mod8 from '../image/8.png'
-// import mod9 from '../image/9.png'
-// function Modules() {
-//   return (
-//     <div className='w-full h-[44rem] bg-white modules_css bg-cover flex flex-col justify-center items-center'>
-//         <div className='h-[20%] w-full flex justify-center items-center pt-20'>
-//                 <p className='text-3xl font-bold text-[#775FB5]'>1 HCM Software, <span className='text-[#FC8320]'>9 HR products,</span> 30+ Modules</p>
-//         </div>
-//         <div className='h-[100%] lg:w-[50%]  grid grid-cols-10 gap-3 grid-row-10 grid-flow-row-dense p-10' >
-//                 <div className='bg-orange-100  h-[100%] rounded-md  col-span-6 row-span-4'>
-//                 <img src={mod1} alt="mod1"  />
-//                 </div>
-                
-//                 <div className='bg-orange-200 col-span-2 row-span-4 rounded-md '>
-//                     <img src={mod2} alt="mod2" />
-//                 </div>
-//                 <div className='bg-orange-300 col-span-2 row-span-4 rounded-md'>
-//                     <img src={mod3} alt="mod3" />
-//                 </div>
-//                 <div className='bg-orange-400 col-span-5 row-span-4 rounded-md'>
-//                     <img src={mod4} alt="mod4" />
-//                 </div>
-//                 <div className='bg-orange-500 col-span-2 row-span-3 rounded-md'>
-//                     <img src={mod5} alt="mod5" />
-//                 </div>
-//                 <div className='bg-orange-600 col-span-3 row-span-3 rounded-md'>
-//                     <img src={mod6} alt="mod6" />
-//                 </div>
-//                 <div className='bg-orange-800 col-span-2 row-span-3 rounded-md'>
-//                     <img src={mod8} alt="mod8" />
-//                 </div>
-//                 <div className='bg-orange-900 col-span-3 row-span-3 rounded-md' >
-//                     <img src={mod9} alt="mod9" />
-//                 </div>
-//                 <div className='bg-orange-700 col-span-5 row-span-2 rounded-md'>
-//                     <img src={mod7} alt="mod7" />
-//                 </div>
-//         </div>  
-//     </div>
-//   )
-// }
-
-// export default Modules
-
-
-// import React from 'react';
-// import mod1 from '../image/1.png';
-// import mod2 from '../image/2.png';
-// import mod3 from '../image/3.png';
-// import mod4 from '../image/4.png';
-// import mod5 from '../image/5.png';
-// import mod6 from '../image/6.png';
-// import mod7 from '../image/7.png';
-// import mod8 from '../image/8.png';
-// import mod9 from '../image/9.png';
-
-// function Modules() {
-//   return (
-//     <div className='w-full h-[44rem] bg-white modules_css bg-cover flex flex-col justify-center items-center'>
-//       <div className='h-[20%] w-full flex justify-center items-center pt-20'>
-//         <p className='text-3xl font-bold text-[#775FB5]'>
-//           1 HCM Software, <span className='text-[#FC8320]'>9 HR products,</span> 30+ Modules
-//         </p>
-//       </div>
-//       <div className='h-[100%] lg:w-[50%]  grid grid-cols-10 gap-3 grid-row-10 grid-flow-row-dense p-10'>
-//         <div className='bg-orange-100 h-[100%] rounded-md col-span-6 row-span-4'>
-//           <img src={mod1} alt="mod1" className="w-full h-full object-cover" />
-//         </div>
-//         <div className='bg-orange-200 col-span-2 row-span-4 rounded-md'>
-//           <img src={mod2} alt="mod2" className="w-full h-full object-cover" />
-//         </div>
-//         <div className='bg-orange-300 col-span-2 row-span-4 rounded-md'>
-//           <img src={mod3} alt="mod3" className="w-full h-full object-cover" />
-//         </div>
-//         <div className='bg-orange-400 col-span-5 row-span-4 rounded-md'>
-//           <img src={mod4} alt="mod4" className="w-full h-full object-cover" />
-//         </div>
-//         <div className='bg-orange-500 col-span-2 row-span-3 rounded-md'>
-//           <img src={mod5} alt="mod5" className="w-full h-full object-cover" />
-//         </div>
-//         <div className='bg-orange-600 col-span-3 row-span-3 rounded-md'>
-//           <img src={mod6} alt="mod6" className="w-full h-full object-cover" />
-//         </div>
-//         <div className='bg-orange-800 col-span-2 row-span-3 rounded-md'>
-//           <img src={mod8} alt="mod8" className="w-full h-full object-cover" />
-//         </div>
-//         <div className='bg-orange-900 col-span-3 row-span-3 rounded-md'>
-//           <img src={mod9} alt="mod9" className="w-full h-full object-cover" />
-//         </div>
-//         <div className='bg-orange-700 col-span-5 row-span-2 rounded-md'>
-//           <img src={mod7} alt="mod7" className="w-full h-full object-cover" />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Modules;
-
-
 import React from 'react';
 import mod1 from '../image/1.png';
 import mod2 from '../image/2.png';
@@ -116,102 +8,35 @@ import mod6 from '../image/6.png';
 import mod7 from '../image/7.png';
 import mod8 from '../image/8.png';
 import mod9 from '../image/9.png';
-import naukri from '../image/naukri.png';
-import linkdin from '../image/linkdin.png';
-import { FaShoppingBag } from "react-icons/fa";
+
 
 function Modules() {
   return (
-    <div className='w-full h-[48rem] bg-white modules_css bg-cover flex flex-col justify-center items-center'>
-      <div className='h-[20%] w-full flex justify-center items-center pt-20'>
-        <p className='text-md sm:text-xl md:text-2xl lgtext-3xl font-bold text-[#775FB5]'>
-          1 HCM Software, <span className='text-[#FC8320]'>9 HR products,</span> 30+ Modules
-        </p>
-      </div>
-      <div className='h-[100%] lg:w-[50%]  grid grid-cols-10 gap-3 place-content-center grid-row-10 grid-flow-row-dense p-10'>
-        <div className=' h-[100%] rounded-md col-span-6 row-span-4'>
-          <img src={mod1} alt="mod1" className="" />
+    <div className='w-full h-screen bg-slate-200 flex flex-col'> 
+        <div className='h-[10%] flex justify-center items-center'>
+          <p className='text-md sm:text-xl md:text-2xl lgtext-3xl font-bold text-[#775FB5]'>
+            1 HCM Software, <span className='text-[#FC8320]'>9 HR products,</span> 30+ Modules
+          </p>
         </div>
-        <div className=' col-span-2 row-span-4 rounded-md'>
-          <img src={mod2} alt="mod2" className="" />
+        <div className='h-[90%] w-full grid grid-flow-row-dense grid-cols-12 grid-rows-12 '>
+                <div className=' col-span-5 bg-orange-100 row-span-6'>
+
+                </div>
+
+                <div className=' col-span-3 bg-orange-200 row-span-6'>
+
+                </div>
+
+                <div className=' col-span-3 bg-orange-300 row-span-6' >
+
+                </div>
+                <div className=' col-span-3 bg-orange-300 row-span-4' >
+
+                </div>
         </div>
-        <div className=' col-span-2 row-span-4 rounded-md'>
-          <img src={mod3} alt="mod3" className="" />
-        </div>
-        <div className=' col-span-5 row-span-4 rounded-md'>
-          <img src={mod4} alt="mod4" className="" />
-        </div>
-        <div className=' col-span-2 row-span-3 rounded-md'>
-          <img src={mod5} alt="mod5" className="" />
-        </div>
-        <div className=' col-span-3 row-span-3 rounded-md'>
-          <img src={mod6} alt="mod6" className="" />
-        </div>
-        <div className=' col-span-2 row-span-3 rounded-md'>
-          <img src={mod8} alt="mod8" className="" />
-        </div>
-        <div className=' col-span-3 row-span-3 rounded-md'>
-          <img src={mod9} alt="mod9" className="" />
-        </div>
-        <div className=' col-span-5 row-span-2 rounded-md'>
-          <img src={mod7} alt="mod7" className="" />
-        </div>
-      </div>
     </div>
   );
-  
-  //   <div className="bg-purple-200 flex flex-col justify-center items-center">
-  //     <div className="pt-20">
-  //       <p className="text-3xl font-bold text-[#775FB5]">
-  //         1 HCM Software, <span className="text-[#FC8320]">9 HR products,</span>{" "}
-  //         30+ Modules
-  //       </p>
-  //     </div>
-  //     <div className="h-[100%] lg:w-[50%]  grid grid-cols-10 gap-4 grid-row-10 grid-flow-row-dense p-10">
-  //       {/*  */}
-  //       <div className="rounded-md col-span-6 row-span-4 bg-white p-10">
-  //         <div className="flex justify-around">
-  //           <div className="">
-  //             <FaShoppingBag className="bg-orange-100 text-orange-500 h-14 w-14 p-2 rounded-md mb-5" />
-  //             <h1 className="text-2xl font-bold">Recruitment Management</h1>
-  //           </div>
-  //           <div className="">
-  //             <img src={naukri} alt="mod1" className="" />
-  //             <br />
-  //             <img src={linkdin} alt="mod1" className="" />
-  //           </div>
-  //         </div>
-  //       </div>
-  //       <div className=" col-span-2 row-span-4 rounded-md bg-red-500">
-  //         <img src={mod2} alt="mod2" className=" " />
-  //       </div>
-  //       <div className=" col-span-2 row-span-4 rounded-md bg-green-500">
-  //         <img src={mod3} alt="mod3" className="" />
-  //       </div>
-  //       {/*  */}
-  //       <div className="col-span-5 row-span-3 rounded-md bg-sky-500 p-10">
-  //         <img src={mod4} alt="mod4" className="" />
-  //       </div>
-  //       <div className=" col-span-2 row-span-1 rounded-md bg-purple-500">
-  //         <img src={mod5} alt="mod5" className="" />
-  //       </div>
-  //       <div className=" col-span-3 row-span-1 rounded-md bg-cyan-500">
-  //         <img src={mod6} alt="mod6" className="" />
-  //       </div>
-  //       {/*  */}
-  //       <div className=" col-span-2 row-span-3 rounded-md bg-gray-500">
-  //         <img src={mod8} alt="mod8" className="" />
-  //       </div>
-  //       <div className=" col-span-3 row-span-3 rounded-md bg-pink-500">
-  //         <img src={mod9} alt="mod9" className="" />
-  //       </div>
-  //       <div className=" col-span-5 row-span-1 rounded-md bg-blue-500">
-  //         <img src={mod7} alt="mod7" className="" />
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
-
+ 
 }
 
 export default Modules;
